800d40eaf17fab894c13c6f3e7dec0f243e3b698
Huong <bella.nguyen@vatek.asia>
2024-12-25 16:57:56 +0700

    KAN-12: Verify categories at the dashboard

--- src/common/scrollTo.js
+++ src/common/scrollTo.js
@@ -1,13 +1,26 @@
+import { By } from "selenium-webdriver";
+
 const scrollToCss = async (driver, css) => {
     await driver.executeScript("window.scrollTo(0, 0);");
     const scrollTo = await driver.findElement(By.css(css));
 
-    await driver.actions().scroll(0, 0, 0, 400, scrollTo).perform();
+    await driver.actions().scroll(0, 0, 0, 500, scrollTo).perform();
+};
+
+const scrollToXpath = async (driver, xpath) => {
+    await driver.executeScript("window.scrollTo(0, 0);");
+    const scrollTo = await driver.findElement(By.xpath(xpath));
+
+    await driver.actions().scroll(0, 0, 0, 700, scrollTo).perform();
 };
 
 const scrollToElement = async (driver, element) => {
-    await driver.executeScript("window.scrollTo(0, 0);");
-    await driver.actions().scroll(0, 0, 0, 400, element).perform();
+    await driver.executeScript("arguments[0].scrollIntoView(true);", element);
 };
 
-export { scrollToCss, scrollToElement };
+// const scrollToElement = async (driver, element) => {
+//     await driver.executeScript("window.scrollTo(0, 0);");
+//     await driver.actions().scroll(0, 0, 0, 400, element).perform();
+// };
+
+export { scrollToCss, scrollToXpath, scrollToElement };
---
+++ src/containers/categories/verifyCategories.spec.js
@@ -0,0 +1,115 @@
+import { By, until } from "selenium-webdriver";
+import { buildDriver } from "../../common/browserBuild.js";
+import { baseURL, timeOut } from "../../common/baseURL.js";
+import assert from "assert";
+import { scrollToCss, scrollToElement, scrollToXpath } from "../../common/scrollTo.js";
+
+const cookieData = [
+  {
+    domain: '.amway.com',
+    expiry: 1766653593,
+    httpOnly: false,
+    name: 'utag_main',
+    path: '/',
+    sameSite: 'Lax',
+    secure: false,
+    value: 'v_id:0193fd115c33000e69d31e5d67570506f004106700bd0$_sn:1$_ss:1$_st:1735119393652$ses_id:1735117593652%3Bexp-session$_pn:1%3Bexp-session'
+  },
+  {
+    domain: '.amway.com',
+    httpOnly: false,
+    name: 'marketLang',
+    path: '/',
+    sameSite: 'Lax',
+    secure: false,
+    value: '%7B%22locale%22%3A%22it-it%22%7D'
+  },
+  {
+    domain: '.amway.com',
+    expiry: 1735121189,
+    httpOnly: false,
+    name: 'academy_user',
+    path: '/',
+    sameSite: 'Lax',
+    secure: false,
+    value: '%7B%22login%22%3A%221600000100717701%22%2C%22FirstName%22%3A%22GENE%22%2C%22LastName%22%3A%22FLUECK%22%7D'
+  },
+  {
+    domain: 'www.qa.esan.academy-preprod.amway.com',
+    expiry: 1735121189,
+    httpOnly: false,
+    name: 'userDetails',
+    path: '/',
+    sameSite: 'Lax',
+    secure: false,
+    value: '%7B%22aboNumber%22%3A%221007177%22%2C%22partyId%22%3A%2242364598%22%2C%22firstName%22%3A%22GENE%22%2C%22lastName%22%3A%22FLUECK%22%2C%22name%22%3A%22GENE%20FLUECK%22%2C%22role%22%3A%22ABO%22%2C%22highestPin%22%3A%22305%22%2C%22salesPlanAff%22%3A%22160%22%2C%22countryCode%22%3A%22IT%22%2C%22amwayId%22%3A%2266901b91-d127-b4af-b645-1fccc63e2bca%22%2C%22showWelcomeVideo%22%3Atrue%2C%22isProspectUser%22%3Afalse%2C%22isProspect2User%22%3Afalse%2C%22aboDetailsToken%22%3A%22%22%2C%22loginId%22%3A%221600000100717701%22%2C%22refreshtoken%22%3A%22%22%2C%22accesstoken%22%3A%22eyJraWQiOiIxZjQwZDk4Mi1iMTJkLTQ5NmEtYTAwMy00MDk3MTk5NDkyNGRfc2lnX3JzMjU2IiwidHlwIjoiSldUIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiI4ODVhNDNmZC04MGIwLTQ1YzEtYmY2ZC0xNjg1MzZjYjY0NjYiLCJjb2RlIjoiNTViYjc1YmUtMjk5OC00YzA2LWIyNjctNDg0M2UzNmJiZDJlIiwiYWJvTnVtYmVyIjoiMTAwNzE3NyIsImlzcyI6Imh0dHBzOi8vYWNjb3VudC1xYS5hbXN0YWNrLWV1LXByZXByb2QuYW13YXkubmV0IiwidG9rZW5fdHlwZSI6ImJlYXJlciIsIm1zcyI6ImFib051bT0xMDA3MTc3IHNhbGVzUGxhbkFmZj0xNjAgcGFydHlJZD00MjM2NDU5OCIsImNsaWVudF9pZCI6IjgxNGQ3MTAxLWY2YjQtNDU0NC1hOTU4LTc3ZGJjOTM4NjI5ZSIsImxvZ2luX3BhcnR5aWQiOiI0MjM2NDU5OCIsImFjY291bnRTdWJUeXBlQ2QiOiJCdXNpbmVzc093bmVyIiwiY3NwYSI6IjE2MCIsImF1ZCI6IjgxNGQ3MTAxLWY2YjQtNDU0NC1hOTU4LTc3ZGJjOTM4NjI5ZSIsIng1dCNTMjU2IjoiIiwic2NvcGUiOlsib3BlbmlkIiwib2ZmbGluZV9hY2Nlc3MiLCJwcm9maWxlIl0sImFjY291bnRUeXBlQ2QiOiJBbXdheUJ1c2luZXNzIiwibXVjIjoiNDIzNjQ1OTgiLCJleHAiOjE3MzUxMjExODksInBhcnR5SWQiOiI0MjM2NDU5OCIsImlhdCI6MTczNTExNzU4OSwiYWNjb3VudCI6eyJjb3VudHJ5IjoiSVQiLCJsTmFtZSI6IkZMVUVDSyIsInJvbGUiOiJCdXNpbmVzc093bmVyIiwiZk5hbWUiOiJHRU5FIiwic2FsZXNfcGxhbl9hZmYiOiIxNjAiLCJsZXZlbCI6IlNpbHZlclNwb25zb3IiLCJyZW5ld2FsRmxhZyI6IjAiLCJsY2xfcGFydHlpZCI6IjQyMzY0NTk4IiwibU5hbWUiOiIiLCJhYm8iOiIxMDA3MTc3Iiwic3RhdHVzIjoiQWN0aXZlIn0sInVzZXJuYW1lIjoiTVBLRjM0SU1URUpTVTRTUUo2VUNPQzZGRlo0Q09QSk9ZV1NFSUlWTVpYTEtJV0VVMkJBUUBleGFtcGxlLmNvbSIsImNpZCI6IjgxNGQ3MTAxLWY2YjQtNDU0NC1hOTU4LTc3ZGJjOTM4NjI5ZSIsInN0YXR1cyI6ImFjdGl2ZSIsImdibF9wYXJ0eWlkIjoiNDIzNjQ1OTgifQ.IigYSkWu93KuOcRKqeJ56uU3mburzGTAfOR5Bn-jefa4VihPmklT0eTxEYQzfTxg9ZQ4nJiyQRm9Je4KtMSlLtDmR2_4BG3yThqcJJq-CpfXpXTvJASc_hUnB0o_C6P2Q0PsSyTJYuKyX8xttNt2KcY0sohL5jmSYzvYaTsDH8KH8aJw4WMepZiKSZnQYq9Y8h4PNnEyqCfB5IwSI8Ily2zpodasxpmy1wBDFKXMSBGDNKxl38WtRe14f298vjl08x56XQ78mMS5zUxGiBVfyhut2owF3sVmCzQ8VM_xupJpKEZ--36QnY-OGHvs6LofPbKt6QJQwVIHQsG3p4d_KA%22%7D'
+  },
+  {
+    domain: '.amway.com',
+    expiry: 1735121189,
+    httpOnly: false,
+    name: 'refreshtoken',
+    path: '/',
+    sameSite: 'Lax',
+    secure: false,
+    value: '%7B%22refreshtoken%22%3A%22cbeec8c9-d013-4ebd-a026-0650206a2640%22%7D'
+  },
+  {
+    domain: '.amway.com',
+    expiry: 1735121189,
+    httpOnly: false,
+    name: 'aboDetailsToken',
+    path: '/',
+    sameSite: 'Lax',
+    secure: false,
+    value: '%7B%22aboDetailsToken%22%3A%22eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhYm8iOiIxMDA3MTc3IiwiYWZmIjoiMTYwIiwicGFydHlJZCI6IjQyMzY0NTk4IiwiaGlnaGVzdEdsb2JhbEF3ZCI6IjMwNSIsImJ0Y0luZm8iOm51bGwsInNpZ25hdHVyZUluZm8iOm51bGwsImNvdW50cnlDb2RlIjoiSVQiLCJhbXdheUlkIjoiNjY5MDFiOTEtZDEyNy1iNGFmLWI2NDUtMWZjY2M2M2UyYmNhIiwiQlRDQ291cnNlc1N0YXR1cyI6bnVsbCwiaWF0IjoxNzM1MTE3NTkzLCJleHAiOjE3MzUxMjExOTN9.PhF6SblpOIP0XoWTvXgnsiN57pWuLnQUg42fNX4vb9Y%22%7D'
+  },
+  {
+    domain: '.amway.com',
+    httpOnly: false,
+    name: 'countryEsanQA',
+    path: '/',
+    sameSite: 'Lax',
+    secure: false,
+    value: '%7B%22country%22%3A%22it%22%7D'
+  }
+]
+
+describe("KAN-12: Verify Categories at the dashboard", () => {
+  let driver;
+  before(async () => {
+    driver = await buildDriver("chrome");
+    await driver.manage().window().maximize();
+  });
+
+  it("Displays the Categories section", async () => {
+    await driver.get(`${baseURL}/dashboard?init=true`);
+    await driver.manage().addCookie(cookieData[0]);
+    await driver.manage().addCookie(cookieData[1]);
+    await driver.manage().addCookie(cookieData[2]);
+    await driver.manage().addCookie(cookieData[3]);
+    await driver.manage().addCookie(cookieData[4]);
+    await driver.manage().addCookie(cookieData[5]);
+
+    await driver.navigate().to(`${baseURL}/dashboard?init=true`);
+
+    //Categories section
+    const element = await driver.wait(until.elementLocated(By.className("dashboard_required_section_wrapper__4WItr")), timeOut);
+    await scrollToElement(driver, element);
+
+    await driver.wait(until.elementLocated(By.className("SectionTitle_section_container__RTAyL")), timeOut);
+    const categoriesTitle = await driver
+      .findElement(By.className("SectionTitle_section_container__RTAyL"))
+      .isDisplayed();
+    assert.equal(categoriesTitle, true, "The Categories section is not displayed");
+
+    await driver.wait(until.elementLocated(By.className("dashboardcategories_wrapper__5udRt")), timeOut);
+    const categoriesContent = await driver
+      .findElement(By.className("dashboardcategories_wrapper__5udRt"))
+      .isDisplayed();
+    assert.equal(categoriesContent, true, "The Categories section is not displayed");
+
+  });
+
+  after(async () => await driver.quit());
+});
