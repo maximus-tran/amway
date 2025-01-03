8cf9cd0417e8893bf8f0010f19ae4f9e77bb09fb
Huong <bella.nguyen@vatek.asia>
2025-01-03 16:57:38 +0700

    KAN-23: Update verify award

--- src/containers/track-progress-award/completedCourse.spec.js
+++ src/containers/track-progress-award/completedCourse.spec.js
@@ -18,31 +18,41 @@ describe("KAN-23: Verify award after finishing course", () => {
       password: "Amway@1234",
       username: "GENE FLUECK",
     }
+    //Login
     await driver
       .wait(until.elementLocated(By.id("loginform:loginid")), timeOut)
       .sendKeys(user.email);
     await driver.findElement(By.id("loginform:password")).sendKeys(user.password);
     await driver.findElement(By.id("loginform:loginButton")).click();
-
     await driver.wait(until.elementLocated(By.css(".tab_wrapper__DEA_E")), timeOut);
+
+    //Navigate to course tab
     const courses = await driver.findElement(By.xpath("//div[@id='__next']/main/div[2]/div/nav/ul/li[2]"));
     await courses.click()
     await driver.wait(until.elementLocated(By.className("courseTabs_tabs_wrapper__LQBEa")), timeOut)
     assert.ok(await driver.getTitle() === "Courses", "Navigation to Courses Page failed");
 
+    //Navigate to in progress section
     await driver.wait(until.elementLocated(By.id("tab-in_progress")), timeOut).click();
     await driver.wait(until.elementLocated(By.id("tabpanel-in_progress")), timeOut);
 
     const listProgress = await driver.findElements(By.css(".tabDetails_cardwrapper_children__9RdHs"));
+    const loopCount = listProgress.length > 3 ? 3 : listProgress.length;
 
-    for (let i = 0; i < listProgress.length; i++) {
+    for (let i = 0; i < loopCount; i++) {
       const awardIcon = await listProgress[i].findElement(By.xpath("//img[@alt='award icon']")).isDisplayed().then(() => true).catch(() => false);
       const timeIcon = await checkElementXpath(driver, "//img[@alt='time icon']");
       const progressBar = await checkElementExists(driver, "card_progressbar_wrapper__36Fzy");
+      //Check course with award icon, time icon and progress bar
       if (awardIcon && timeIcon && progressBar) {
         const originalWindow = await driver.getWindowHandle();
-        await listProgress[i].click();
+        await listProgress[2].click();
         const titleCourse = await driver.wait(until.elementLocated(By.className("startLaunchCourse_main_title__VMVRr")), timeOut).getText();
+        console.log('====================================');
+        console.log(titleCourse);
+        console.log('====================================');
+        await driver.wait(until.elementLocated(By.className("startLaunchCourse_cross_icon__eyRob")), timeOut).click();
+        //Start course
         const button = await driver.wait(until.elementLocated(By.className("courseDurationModal_btn_container__NANdK")), timeOut);
         await button.click();
 
@@ -63,7 +73,6 @@ describe("KAN-23: Verify award after finishing course", () => {
         if (iframe.length > 0) {
           await driver.switchTo().frame(iframe[0]);
         }
-
         const video = await checkElementXpath(driver, "//video[@id='video_html5_api']");
         if (video) {
           const videoElement = await driver.findElement(By.xpath("//video[@id='video_html5_api']"));
@@ -77,37 +86,27 @@ describe("KAN-23: Verify award after finishing course", () => {
         await driver.close();
         await driver.switchTo().window(originalWindow);
 
-        await driver.sleep(8000);
+        await driver.sleep(1000);
         //Course should be removed removed from In Progress tab
         await driver.wait(until.elementLocated(By.id("tab-in_progress")), timeOut).click();
-        const title = await driver.wait(until.elementLocated(By.className("card_name__HOcFA")), timeOut).getText();
-        assert.ok(titleCourse === !title, "The course is still present in the In Progress section");
-        
+        await driver.wait(until.elementLocated(By.id("tabpanel-in_progress")), timeOut);
+        const title = await driver.findElements(By.className("card_name__HOcFA"));
+        const loopProgressCount = title.length > 3 ? 3 : title.length;
+        for (let index = 0; index < loopProgressCount; index++) {
+          const courseTitle = await title[index].getText();
+          assert.ok(titleCourse !== courseTitle, 'The course is still present in the In Progress section');
+        }
+
         //Course should be displayed at "Complete" tab
         await driver.wait(until.elementLocated(By.id("tab-completed")), timeOut).click();
         await driver.wait(until.elementLocated(By.id("tabpanel-completed")), timeOut);
-        const listCompleted = await driver.findElements(By.css(".tabDetails_cardwrapper_children__9RdHs"));
+        const listCompleted = await driver.findElements(By.className("card_name__HOcFA"));
 
-        let courseCompleted = false;
-        for (let i = 0; i < listCompleted.length; i++) {
-          await listCompleted[i].click();
-          const titleCompleted = await driver.wait(until.elementLocated(By.className("startLaunchCourse_main_title__VMVRr")), timeOut).getText();
+        for (let x = 0; x < listCompleted.length; x++) {
+          const titleCompleted = await listCompleted[x].getText();
           if (titleCourse === titleCompleted) {
-            const retakeCourse = await driver.wait(until.elementLocated(By.className("button_btn_text__rvpWC")), timeOut).getText();
-            const langArr = [
-              { label: "Ripeti il corso" },
-              { label: "Retake Course" },
-              { label: "重修课程" },
-            ]
-            const buttonValue = langArr.find(x => x.label === retakeCourse);
-            assert.strictEqual(retakeCourse, buttonValue.label, `${retakeCourse} and ${buttonValue} not match`)
-
-            const completedIcon = await driver.wait(until.elementLocated(By.xpath("//img[@alt='done symbol']")), timeOut);
-            assert.ok(await completedIcon.isDisplayed(), "Completion icon is not displayed");
-
-            const achievementIcon = await driver.wait(until.elementLocated(By.xpath("//img[@alt='achievement icon']")), timeOut);
-            assert.ok(await achievementIcon.isDisplayed(), "Achievement icon is not displayed");
-
+            await listCompleted[x].click();
+            //Check award card is displayed in pop up
             const img = await driver.wait(until.elementLocated(By.className("awardModal_image_wrapper__zoJSV")), timeOut);
             const titleAward = await driver
               .wait(until.elementLocated(By.className("awardModal_awardtitle__7ethH")), timeOut)
@@ -116,23 +115,25 @@ describe("KAN-23: Verify award after finishing course", () => {
             assert.ok(await img.isDisplayed(), "Award image is not displayed");
             assert.ok(await titleAward.isDisplayed(), `${titleAward} is not displayed`);
             assert.ok(await descriptionAward.isDisplayed(), `${descriptionAward} is not displayed`);
+            //Open award card
             await driver.wait(until.elementLocated(By.className("awardModal_awardContainer__xf2lG awardModal_awardCompleted__kM1Oh")), timeOut).click();
             await driver.wait(until.elementLocated(By.className("courseCompletionWithAward_courseCompletionWithAwardContainer__LfrdE")), timeOut).isDisplayed();
             const titlePopup = await driver.findElement(By.className("courseCompletionWithAward_courseName__sn4EG")).getText();
+            assert.ok(await titleAward.getText() === titlePopup, `${titleAward} and ${titlePopup} not match`);
             const usernameXpath = `//div[@aria-label='abo name - ${user.username}']`;
             const username = await driver.findElement(By.xpath(usernameXpath)).getText();
-            assert.ok(await titleAward.getText() === titlePopup, `${titleAward} and ${titlePopup} not match`);
             assert.ok(username === user.username, `${username} and ${user.username} not match`);
+            //Close award pop up
             await driver.findElement(By.className("icon courseCompletionWithAward_cross_icon__OBd0_")).click();
+            //Close course pop up
             await driver.wait(until.elementLocated(By.className("startLaunchCourse_cross_icon__eyRob")), timeOut).click();
             break;
           }
-          await driver.wait(until.elementLocated(By.className("startLaunchCourse_cross_icon__eyRob")), timeOut).click();
+          else if (!titleCompleted) {
+            console.log("Course not completed")
+          }
         }
-        if (!courseCompleted) {
-          console.log("Course not completed");
-          break;
-        }
+        break;
       }
       await driver.wait(until.elementLocated(By.className("icon startLaunchCourse_cross_icon__eyRob")), timeOut).click();
     }
